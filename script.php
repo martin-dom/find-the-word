510c9fd28c7e0679f1e949b757f4b9cd8aa9e635
martin-dom <martin-dominik@email.cz>
2024-08-22 11:24:52 +0200

    Update script.php

--- script.php
+++ script.php
@@ -1,8 +1,11 @@
 <?php
-$searchWord = 'life';
+$searchWords = ['house','apple','table','chair','bread','water', 'window','plant','world','ocean','river','storm','stone','cloud','money','music','clock','light','train','horse','pizza','movie','dream','tiger','store','dance','bread','fruit','tiger','brain','heart','beach','night','sugar','magic','fire','smile','glass','paper','child','grass'];
+$x = rand(0, count($searchWords) - 1);
 
 session_start();
-
+if(!isset($_SESSION['searchWord'])){
+    $_SESSION['searchWord'] = [$searchWords[$x]];
+}
 if (!isset($_SESSION['all_attempts'])) {
     $_SESSION['all_attempts'] = [];
 }
@@ -10,19 +13,22 @@ if (!isset($_SESSION['all_attempts'])) {
 # capturing the form submission
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     # catch the attempt
-    $attempt = $_POST['attempt'];
+    $attempt = strtolower($_POST['attempt']);
 
+    
     echo '<div class="attempts">';
+    
     # showing previous attempts
     for($i = 0; $i < count($_SESSION['all_attempts']); $i++){
-        compare($_SESSION['all_attempts'][$i], $searchWord);
+        compare($_SESSION['all_attempts'][$i], $_SESSION['searchWord'][0]);
     }
-
+    
     # store attempt to all_attempts
     $_SESSION['all_attempts'][] = $attempt;
-
+    
     # new attempt compare
-    compare($attempt, $searchWord);
+    compare($attempt, $_SESSION['searchWord'][0]);
+    
 }
 
 
@@ -31,6 +37,7 @@ function compare($attempt, $searchWord){
     if($attempt === $searchWord){
         echo '<h1/>Congratulations, you won!</h1>';
         echo '<h3/> Right word is ' . $attempt . '</h3>'; 
+        echo '<h4/> You had ' . count($_SESSION['all_attempts']) . ' attempts</h4>';
         session_destroy();
     }else{
         error_reporting(E_ALL & ~E_WARNING); #set no warning
@@ -47,8 +54,6 @@ function compare($attempt, $searchWord){
         echo '</div>';
         error_reporting(E_ALL); #set warning back
     }
-    echo $attempt;
 }
 echo '</div>';
-
 ?>
